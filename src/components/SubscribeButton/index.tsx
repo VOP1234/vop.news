04072e7afbc92c18b0f73e495201e3e108df6002
Vitor Pereira <vop1234@hotmail.com>
2021-04-19 05:47:39 -0300

    Front-end JAMStack

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,5 @@
-import { useSession } from 'next-auth/client'
+import { signIn, useSession } from 'next-auth/client'
+import { useRouter } from 'next/router'
 import { api } from '../../services/api'
 import { getStripeJS } from '../../services/stripe-js'
 import styles from './styles.module.scss'
@@ -9,10 +10,18 @@ interface SubscribeButtonProps {
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [session] = useSession()
+  const router = useRouter()
 
   async function handleSubscribe() {
     if (!session) {
-      throw new Error('Usuário não logado.')
+      signIn('github')
+      return
+    }
+
+
+    if (session.activeSubscription) {
+      router.push('/post')
+      return
     }
 
     try {
